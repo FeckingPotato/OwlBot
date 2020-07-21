e0537d63ad34844006f9a131b10d8f113b0cd420
Potato <cornilow.serega@gmail.com>
2020-07-21 14:37:28 +0700

    moved admin commands to a separate file

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const mongo_client = new MongoClient(process.env.URI, {useUnifiedTopology: true}
 
 const fun_cmd = require('./reqs/fun_commands.js')
 const ecn_cmd = require('./reqs/economy_commands.js')
+const adm_cmd = require('./reqs/admin_commands.js')
 const help = require('./reqs/help.js').help
 const database = require('./reqs/database.js')
 const http = require('./reqs/http-functions.js')
@@ -17,50 +18,46 @@ var cooldown_owl = 0
 http.owl()
 
 mongo_client.connect(() => {
-discord_client.login(token)
-console.log('working')
-discord_client.on('message', msg => {
-	if ((msg.content.startsWith('!')) || (msg.content.startsWith('*'))) {
-		switch (msg.content.split(' ')[0]) {
-			case '!money':
-				ecn_cmd.money(msg, mongo_client)
-				break
-			case '!daily':
-				ecn_cmd.daily(msg, mongo_client)
-				break
-			case '!owl':
-				if (cooldown_owl === 0) {
-					fun_cmd.owl(msg)
-					cooldown_owl = 1
-					setTimeout(() => cooldown_owl = 0, 2000)
-				}
-				break
-			case '!rr':
-				fun_cmd.rr(msg, mongo_client)
-				break
-			case '!prb':
-				fun_cmd.prb(msg, mongo_client)
-				break
-			case '!egg':
-				fun_cmd.egg(msg, mongo_client)
-				break
-			case '!help':
-				help(msg, mongo_client)
-				break
-			case '*lang':
-				if (msg.member.hasPermission("ADMINISTRATOR")) {
-					switch (msg.content.split(' ')[1]) {
-						case 'en':
-							database.setValue(mongo_client, msg.guild.id, 'language', 'en')
-							break
-						case 'ru':
-							database.setValue(mongo_client, msg.guild.id, 'language', 'ru')
-							break
+	discord_client.login(token)
+	console.log('working')
+	discord_client.on('message', msg => {
+		if (msg.content.startsWith('!')) {
+			switch (msg.content.split(' ')[0]) {
+				case '!money':
+					ecn_cmd.money(msg, mongo_client)
+					break
+				case '!daily':
+					ecn_cmd.daily(msg, mongo_client)
+					break
+				case '!owl':
+					if (cooldown_owl === 0) {
+						fun_cmd.owl(msg)
+						cooldown_owl = 1
+						setTimeout(() => cooldown_owl = 0, 2000)
 					}
-				}
-				break
-		
-}}})})
+					break
+				case '!rr':
+					fun_cmd.rr(msg, mongo_client)
+					break
+				case '!prb':
+					fun_cmd.prb(msg, mongo_client)
+					break
+				case '!egg':
+					fun_cmd.egg(msg, mongo_client)
+					break
+				case '!help':
+					help(msg, mongo_client)
+					break
+			}
+		}
+		else if ((msg.content.startsWith('*')) || (msg.member.hasPermission("ADMINISTRATOR"))) {
+			switch (msg.content.split(' ')[0]) {
+				case '*lang':
+					adm_cmd.language(msg, mongo_client)
+			}
+		}
+	})
+})
 
 const express = require('express')
 
---
+++ reqs/admin_commands.js
@@ -0,0 +1,16 @@
+const database = require('./database.js')
+
+async function language(msg, mongo_client) {
+    switch (msg.content.split(' ')[1]) {
+        case 'en':
+            database.setValue(mongo_client, msg.guild.id, 'language', 'en')
+            msg.channel.send('The language is English now')
+            break
+       case 'ru':
+            database.setValue(mongo_client, msg.guild.id, 'language', 'ru')
+            msg.channel.send('The language is Russian now')
+            break
+    }
+}
+
+module.exports.language = language
