66521487114cfc0985a2796cde034c39149a7b07
Potato <cornilow.serega@gmail.com>
2020-08-10 00:53:27 +0700

    fixed the multiple space bug in !pay

--- reqs/user_commands.js
+++ reqs/user_commands.js
@@ -175,7 +175,7 @@ async function pay(msg, mongo_client) {
 	let lang = await database.getValue(mongo_client, msg.channel.id, 'language')
 	let payer = await msg.member.user
 	let paid = await msg.mentions.users.first()
-	let money = parseInt(await msg.content.split(' ')[2])
+	let money = parseInt(await msg.content.split(' ').reverse()[0])
 	if (paid === undefined) msg.channel.send(eval(`${lang}.pay_person`))
 	else if (money === undefined) msg.channel.send(eval(`${lang}.pay_money`))
 	else if (money % 1 !== 0 || money < 0) msg.channel.send(eval(`${lang}.pay_integer`))
