fab64d0d028d0a23ab8e2ed67c8a134039e8f3f1
Potato <cornilow.serega@gmail.com>
2020-08-16 13:03:26 +0700

    removed dumb eval functions

--- reqs/user_commands.js
+++ reqs/user_commands.js
@@ -2,15 +2,14 @@ const {MessageAttachment} = require('discord.js')
 const fs = require('fs')
 const http = require('./http-functions.js')
 const database = require('./database.js')
-const ru = JSON.parse(fs.readFileSync('./translations/ru.json'))
-const en = JSON.parse(fs.readFileSync('./translations/en.json'))
+const translation = JSON.parse(fs.readFileSync('./reqs/translation.json'))
 
 async function help(msg, mongo_client) {
 	let lang = await database.getValue(mongo_client, msg.channel.id, 'language')
 	try {
 		let command = msg.content.split(' ')[1]
-		if (command === undefined) msg.channel.send(eval(`${lang}.help_main`))
-		else msg.channel.send(eval(`${lang}.help_${command}`))
+		if (command === undefined) msg.channel.send(translation[lang].help_main)
+		else msg.channel.send(eval(`translation[lang].help_${command}`))
 	}
 	catch (error) {console.log(error)}
 }
@@ -25,31 +24,31 @@ async function rr(msg, mongo_client) {
 	let lang = await database.getValue(mongo_client, msg.channel.id, 'language')
 	let ded_role = msg.guild.roles.cache.find(role => role.name === 'ded')
 	let ded_role_member = msg.member.roles.cache.find(role => role.name === 'ded')
-	if (ded_role === undefined) msg.channel.send(eval(`${lang}.rr_undefined`))
+	if (ded_role === undefined) msg.channel.send(translation[lang].rr_undefined)
 	else {
-		if (ded_role_member) msg.reply(eval(`${lang}.rr_dead`))	
+		if (ded_role_member) msg.reply(translation[lang].rr_dead)	
 		else {
 			if (Math.floor(Math.random()*6) == 0) {
-			msg.reply(eval(`${lang}.rr_loss`))
+			msg.reply(translation[lang].rr_loss)
 			msg.member.roles.add(ded_role)
 			setTimeout(() => msg.member.roles.remove(ded_role), 120000)
 			}
-			else msg.reply(eval(`${lang}.rr_win`))
+			else msg.reply(translation[lang].rr_win)
 		}
 	}
 }
 
 async function prb(msg, mongo_client) { //real
 	let lang = await database.getValue(mongo_client, msg.channel.id, 'language')
-	if (msg.content === '!prb') msg.reply(eval(`${lang}.prb_nothing`))
-	else msg.reply(eval(`${lang}.prb_something`)+Math.floor(Math.random()*100)+'%')
+	if (msg.content === '!prb') msg.reply(translation[lang].prb_nothing)
+	else msg.reply(translation[lang].prb_something + Math.floor(Math.random()*100)+'%')
 }
 
 async function рrb(msg, mongo_client) { //fake
 	if (msg.member.hasPermission("ADMINISTRATOR")) {
 		let lang = await database.getValue(mongo_client, msg.channel.id, 'language')
-		if (msg.content === '!prb') msg.reply(eval(`${lang}.prb_nothing`))
-		else msg.reply(eval(`${lang}.prb_something`)+Math.floor(Math.random()*(100-80)+80)+'%')
+		if (msg.content === '!prb') msg.reply(translation[lang].prb_nothing)
+		else msg.reply(translation[lang].prb_something + Math.floor(Math.random()*(100-80)+80)+'%')
 	}
 }
 
@@ -57,7 +56,7 @@ async function egg(msg, mongo_client) {
 	let lang = await database.getValue(mongo_client, msg.channel.id, 'language')
 	let egger = msg.member.user
 	let egged = msg.mentions.users.first()
-	if (egged === undefined) msg.channel.send(msg.member.user.username+eval(`${lang}.egg_undefined`))
+	if (egged === undefined) msg.channel.send(egger.username + translation[lang].egg_undefined)
 	else {
 		egg_count = await database.getValue(mongo_client, egged.id, 'egg-count')
 		if (egg_count === (NaN||null||undefined)) {
@@ -68,8 +67,8 @@ async function egg(msg, mongo_client) {
 			database.incValue(mongo_client, egged.id, 'egg-count', 1)
 			egg_count = egg_count + 1
 		}
-		if (egger.id === egged.id) msg.channel.send(egger.username+eval(`${lang}.egg_self`)+egged.username+': '+egg_count)
-		else msg.channel.send(egger.username+eval(`${lang}.egg_someone1`)+egged.username+eval(`${lang}.egg_someone2`)+egged.username+': '+egg_count)
+		if (egger.id === egged.id) msg.channel.send(egger.username + translation[lang].egg_self + egged.username + ': ' + egg_count)
+		else msg.channel.send(egger.username+translation[lang].egg_someone1 + egged.username + translation[lang].egg_someone2 + egged.username+': ' + egg_count)
 	}
 }
 
@@ -94,7 +93,7 @@ function msToTime(s, lang) {
 		if (secs == 1) {var secs_string = ' second '} else {var secs_string = ' seconds '}
 	}
 	let result = ''
-	if ((hrs == 0) && (mins == 0) && (secs == 0)) {result = eval(`${lang}.mstotime_less`)}
+	if ((hrs == 0) && (mins == 0) && (secs == 0)) {result = translation[lang].mstotime_less}
 	if (hrs > 0) {result = result + hrs + hrs_string}
 	if (mins > 0) {result = result + mins + mins_string}
 	if (secs > 0) {result = result + secs + secs_string}
@@ -105,7 +104,7 @@ async function money(msg, mongo_client) {
 	let lang = await database.getValue(mongo_client, msg.channel.id, 'language')
 	let money = await database.getValue(mongo_client, msg.member.user.id, 'money')
 	if (money === undefined) {money = 0}
-	let response = eval(`${lang}.money_reply`) + money
+	let response = translation[lang].money_reply + money
 	msg.reply(response)
 }
 
@@ -119,20 +118,20 @@ async function daily(msg, mongo_client) {
 		database.setValue(mongo_client, rewarded, 'daily-cooldown', cooldown_exp)
 		if (money === undefined) database.setValue(mongo_client, rewarded, 'money', 0)
 		database.incValue(mongo_client, rewarded, 'money', 100)
-		msg.channel.send(eval(`${lang}.daily_reward`))
+		msg.channel.send(translation[lang].daily_reward)
 	}
 	else {
 		let time_left = msToTime(await database.getValue(mongo_client, rewarded, 'daily-cooldown')-Date.now(), lang)
-		msg.channel.send(eval(`${lang}.daily_left`)+time_left)
+		msg.channel.send(translation[lang].daily_left + time_left)
 	}
 }
 
 async function shop(msg, mongo_client) {
 	let lang = await database.getValue(mongo_client, msg.channel.id, 'language')
 	let roles_arr = await database.getValue(mongo_client, msg.guild.id, 'role_prices')
-	if (roles_arr === undefined ^ []) msg.channel.send(eval(`${lang}.shop_unavailable`))
+	if (roles_arr === undefined ^ []) msg.channel.send(translation[lang].shop_unavailable)
 	else {
-		let list = eval(`${lang}.shop_available`)
+		let list = translation[lang].shop_available
 		for (i = 0; i < roles_arr.length; i++) {
 			let role = await msg.guild.roles.fetch(roles_arr[i].role_id)
 			let role_price = await roles_arr[i].role_price
@@ -144,16 +143,16 @@ async function shop(msg, mongo_client) {
 
 async function buy_role(msg, mongo_client) {
 	let lang = await database.getValue(mongo_client, msg.channel.id, 'language')
-	let msg_array = msg.content.split('"')
+	let msg_array = msg.content.split(' ')
 	msg_array.splice(0, 1)
 	let role_name = msg_array.join(' ')
-	if (role_name === '') msg.reply(eval(`${lang}.buy_undefined`))
+	if (role_name === '') msg.reply(translation[lang].buy_undefined)
 	else {
 		role = msg.guild.roles.cache.find(role => role.name === role_name)
-		if (role === undefined) msg.reply(eval(`${lang}.buy_nonexistent1`)+`${'`'}${role_name}${'`'}`+eval(`${lang}.buy_nonexistent2`))
+		if (role === undefined) msg.reply(translation[lang].buy_nonexistent1 + `${'`'}${role_name}${'`'}` + translation[lang].buy_nonexistent2)
 		else {
 			let has_role = msg.member.roles.cache.find(role => role.name === role_name)
-			if (has_role !== undefined) msg.reply(eval(`${lang}.buy_owned`))
+			if (has_role !== undefined) msg.reply(translation[lang].buy_owned)
 			else {
 				let roles_db = await database.getValue(mongo_client, msg.guild.id, 'role_prices')
 				let money = await database.getValue(mongo_client, msg.member.user.id, 'money')
@@ -166,14 +165,14 @@ async function buy_role(msg, mongo_client) {
 							try {
 								await msg.member.roles.add(role)
 								await database.incValue(mongo_client, msg.member.user.id, 'money', payment)
-								await msg.channel.send(`${msg.member}`+eval(`${lang}.buy_success`)+`${'`'}${role_name}${'`'}`)
+								await msg.channel.send(`${msg.member}` + translation[lang].buy_success + `${'`'}${role_name}${'`'}`)
 							}
-							catch {msg.reply(eval(`${lang}.buy_error`))}
+							catch {msg.reply(translation[lang].buy_error)}
 						}
-						else msg.reply(eval(`${lang}.notenoughcashstranger`))
+						else msg.reply(translation[lang].notenoughcashstranger)
 					}
 				}
-				if (buyable === undefined) msg.reply(eval(`${lang}.buy_unavailable`))
+				if (buyable === undefined) msg.reply(translation[lang].buy_unavailable)
 			}
 		}
 	}
@@ -184,16 +183,16 @@ async function pay(msg, mongo_client) {
 	let payer = await msg.member.user
 	let paid = await msg.mentions.users.first()
 	let money = parseInt(await msg.content.split(' ').reverse()[0])
-	if (paid === undefined) msg.channel.send(eval(`${lang}.pay_person`))
-	else if (money === undefined) msg.channel.send(eval(`${lang}.pay_money`))
-	else if (money % 1 !== 0 || money < 0) msg.channel.send(eval(`${lang}.pay_integer`))
+	if (paid === undefined) msg.channel.send(translation[lang].pay_person)
+	else if (money === undefined) msg.channel.send(translation[lang].pay_money)
+	else if (money % 1 !== 0 || money < 0) msg.channel.send(translation[lang].pay_integer)
 	else {
 		let money_payer = await database.getValue(mongo_client, payer.id, 'money')
-		if (money_payer < money) msg.reply(eval(`${lang}.notenoughcashstranger`))
+		if (money_payer < money) msg.reply(translation[lang].notenoughcashstranger)
 		else {
 			await database.incValue(mongo_client, paid.id, 'money', money)
 			await database.incValue(mongo_client, payer.id, 'money', -money)
-			msg.reply(`${eval(`${lang}.pay_success`)}${'`'}${paid.username}${'`'} ₴‎${money}`)
+			msg.reply(`${translation[lang].pay_success}${'`'}${paid.username}${'`'} ₴‎${money}`)
 		}
 	}
 }
@@ -219,10 +218,10 @@ async function top (msg, mongo_client) {
 	})
 	let people_count = 5
 	if (money_obj.length < 5) people_count = money_obj.length
-	if (people_count == 0) msg.channel.send(eval(`${lang}.top_communism`))
-	else if (people_count == 1) msg.channel.send(`${eval(`${lang}.top_oneperson`)} ${money_obj[0].name} - ₴${money_obj[0].money}`)
+	if (people_count == 0) msg.channel.send(translation[lang].top_communism)
+	else if (people_count == 1) msg.channel.send(`${translation[lang].top_oneperson} ${money_obj[0].name} - ₴${money_obj[0].money}`)
 	else {
-		let result = `${eval(`${lang}.top_people1`)} ${people_count} ${eval(`${lang}.top_people2`)}`
+		let result = `${translation[lang].top_people1} ${people_count} ${translation[lang].top_people2}`
 		for (var i = 0; i < people_count; i++) {
 			result = result + `${i+1}. ${money_obj[i].name} - ₴${money_obj[i].money}\n`
 		}
