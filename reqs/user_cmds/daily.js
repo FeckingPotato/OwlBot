6ca4d4862151e00bdcf2591459bb0bb59bf5bb63
Sergey <potato@pop-os.localdomain>
2020-09-21 12:49:17 +0700

    Added daily reward progression

--- reqs/user_cmds/daily.js
+++ reqs/user_cmds/daily.js
@@ -7,11 +7,38 @@ module.exports = async function daily(msg, mongo_client) {
 	let money = await database.getValue(mongo_client, rewarded, 'money');
 	let daily_cooldown = await database.getValue(mongo_client, rewarded, 'daily-cooldown');
 	if (daily_cooldown === undefined || daily_cooldown <= Date.now()) {
+		if ((Date.now() - daily_cooldown) > 86400000) database.setValue(mongo_client, rewarded, 'daily-tier', 0);
 		let cooldown_exp = Date.now() + 86400000;
 		database.setValue(mongo_client, rewarded, 'daily-cooldown', cooldown_exp);
+		let daily_tier = await database.getValue(mongo_client, rewarded, 'daily-tier');
 		if (money === undefined) database.setValue(mongo_client, rewarded, 'money', 0);
-		database.incValue(mongo_client, rewarded, 'money', 100);
-		msg.channel.send(translation[lang].daily_reward);
+		let reward = 100;
+		switch(daily_tier) {
+			default:
+				database.setValue(mongo_client, rewarded, 'daily-tier', 1);
+				break;
+			case 1:
+				reward = 150;
+				database.incValue(mongo_client, rewarded, 'daily-tier', 1);
+				break;
+			case 2:
+				reward = 200;
+				database.incValue(mongo_client, rewarded, 'daily-tier', 1);
+				break;
+			case 3:
+				reward = 250;
+				database.incValue(mongo_client, rewarded, 'daily-tier', 1);
+				break;
+			case 4:
+				reward = 300;
+				database.incValue(mongo_client, rewarded, 'daily-tier', 1);
+				break;
+			case 5:
+				reward = 350;
+				break;
+		};
+		database.incValue(mongo_client, rewarded, 'money', reward);
+		msg.channel.send(translation[lang].daily_reward1 + String(reward) + translation[lang].daily_reward2);
 	} else {
 		let time_left = require('../mstotime.js')((await database.getValue(mongo_client, rewarded, 'daily-cooldown')) - Date.now(),lang);
 		msg.channel.send(translation[lang].daily_left + time_left);
